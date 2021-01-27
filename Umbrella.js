8e93ba9757ebaca0623208302923b9349f2da107
Ananya22oct <75477416+Ananya22oct@users.noreply.github.com>
2021-01-27 13:08:47 +0300

    Add files via upload

---
+++ Umbrella.js
@@ -0,0 +1,18 @@
+class Umbrella{
+    constructor(x,y){
+        options={
+            isStatic:true
+        }
+
+        this.image=loadImage("images/walking_1.png");
+        this.body=Bodies.circle(x,y,50,options);
+        this.radius=50;
+        World.add(world,this.body);
+    }
+    display(){
+        var pos=this.body.position;
+
+        imageMode(CENTER);
+        image(this.image,pos.x,pos.y,400,400);
+    }
+}
